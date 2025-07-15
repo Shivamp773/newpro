b123bff11b1b3ed5a24d95f014acb26dc1ffbbd1
Shivamp773 <spandey.7773@gmail.com>
2025-07-15 09:36:28 +0530

    Update API.js

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_BASEURL });
+const API = axios.create({
+  baseURL: "https://newpro-green.vercel.app/",
+  withCredentials: true, 
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
